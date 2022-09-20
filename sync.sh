ecd5ee09cab8bfa0cc1850c26bcd0420fae2bcb7
ian <ian@DESKTOP-RJKHJQK.localdomain>
2022-09-20 01:57:03 -0400

    publish

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ian/code/memesbowl.github.io-master
-/usr/bin/python script.py
+/usr/bin/python /home/ian/code/memesbowl.github.io-master/script.py
 git add .
 git commit -m "publish"
 git push
