66211d383446778ea00466936a80454be835c06d
ian <ian@DESKTOP-RJKHJQK.localdomain>
2023-09-11 18:42:46 -0400

    publish

--- sync.sh
+++ sync.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /home/ihrm/memesbowl.github.io
-python /home/ihrm/memesbowl.github.io/script.py
-git add .
-git commit -m "publish"
-git push
+current_day=$(date +%u)
+
+if [ "$current_day" -eq 1 ]; then
+    cd /home/ihrm/memesbowl.github.io
+    python /home/ihrm/memesbowl.github.io/script.py
+    git add .
+    git commit -m "publish"
+    git push
+    echo "pushed scoreboard update for week"
+else
+    echo "not tuesday do not update"
+fi
